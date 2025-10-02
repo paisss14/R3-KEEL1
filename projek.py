2723d4e0f51035daf1508ea8f0ccb596e0970293
AmdSidiq <ahmadsidiq796@gmail.com>
2025-10-02 22:10:29 +0700

    test sidiq

--- projek.py
+++ projek.py
@@ -1,1 +1,12 @@
-print ("masih proses kak")
+# Ngetes aja ini bang ntar dihapus aja wkwkw
+n = int(input())
+
+for i in range(1,n + 1):
+    if i == n:
+        print("Kapal Karam")
+    elif i % 3 == 0 and i % 5 == 0:
+        continue
+    elif i % 11 == 0:
+        print("DPA Asiq")
+    else:
+        print(i)
