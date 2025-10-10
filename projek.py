2bfe3889f1641b4fd57649526f7ced6f15278394
AmdSidiq <ahmadsidiq796@gmail.com>
2025-10-10 10:29:15 +0700

    nyoba

--- projek.py
+++ projek.py
@@ -1,4 +1,3 @@
-# Ngetes aja ini bang ntar dihapus aja wkwkw
 n = int(input("Masukkan angka : "))
 for i in range(1,n + 1):
     if i == n:
@@ -9,3 +8,4 @@ for i in range(1,n + 1):
         print("DPA Asiq")
     else:
         print(i)
+#tambahan aja
