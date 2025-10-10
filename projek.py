3f9ceae800a0cbbd027dda27f27e650727bfa3d7
AmdSidiq <ahmadsidiq796@gmail.com>
2025-10-10 11:45:44 +0700

    tes

--- projek.py
+++ projek.py
@@ -12,3 +12,13 @@ for i in range(1,n + 1):
 
 print("Hello World")
 
+n = int(input("Masukkan angka : "))
+for i in range(1,n + 1):
+    if i == n:
+        print("Kapal Karam")
+    elif i % 3 == 0 and i % 5 == 0:
+        continue
+    elif i % 11 == 0:
+        print("DPA Asiq")
+    else:
+        print(i)
