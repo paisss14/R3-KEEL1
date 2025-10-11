4dfd6c02427c73df0217024c933c1b759f853d63
paisss14 <restufaiz14@gmail.com>
2025-10-11 08:59:39 +0700

    finishing.py

--- projek.py
+++ projek.py
@@ -1,8 +1,6 @@
-# ini fungsi buat ngitung diskonnya
 def hitung_diskon(harga, diskon):
-    return harga - (harga * diskon // 100)
+    return harga - (harga * diskon / 100)
 
-# ini fungsi buat nentuin kategori diskon
 def kategori_diskon(diskon):
     if diskon <= 0:
         return "tidak dapat diskon"
@@ -13,14 +11,12 @@ def kategori_diskon(diskon):
     else:
         return "🎉 Hemat besar!"
 
-# ini buat inputan sama ngehitung harga akhir + diskon nya
 harga_awal = int(input("Masukkan harga barang: Rp "))
 diskon = int(input("Masukkan diskon (%): "))
 nama_barang = input("masukkan nama barang mu: ")
 harga_akhir = hitung_diskon(harga_awal, diskon)
 harga_diskon = harga_awal - harga_akhir
 
-# ini hasil akhir yng bakal di cetak semuanye
 print("===== Kalkulator Diskon =====")
 print(f"📦 Barang = {nama_barang}")
 print(f"💵 Harga awal = {harga_awal}")
